f576d8e4b0a99eaaeaa8155dc719d347243ef7e7
Lvthr <martimoa@stud.iie.ntnu.no>
2018-02-28 14:08:54 +0100

    Made changes to MyApp.java to TestBranch

--- src/myapp/MyApp.java
+++ src/myapp/MyApp.java
@@ -14,5 +14,9 @@ public class MyApp {
     public static void main(String[] args) throws Exception {
         MyWindow window = new MyWindow("The Window");
         window.setVisible(true);
+        /*
+        * Dette er en kommentar jeg (Martin) la til i branchen TestBranch
+        */
+        System.out.println("Dette er en testendring");
    }  
 }
