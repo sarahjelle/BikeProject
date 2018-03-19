7882983924c062d5c3bbc183b0beb02e7c513102
Martin Moan <martimoa@stud.iie.ntnu.no>
2018-03-19 14:58:13 +0100

    Add User class

---
+++ src/myapp/data/User.java
@@ -0,0 +1,60 @@
+package myapp.data;
+
+public class User {
+    final static int ADMINISTRATOR = 0;
+    final static int REPERATØR = 1;
+    final static int KUNDE = 2;
+
+    private final int UserID;
+    private final int UserClass;
+
+    private final String firstname;
+    private final String lastname;
+    private final int phone;
+    private final String email;
+    //private final int landcode;
+
+    public User(int UserClass, int UserID, String firstname, String lastname, int phone, String email){
+        this.UserID = UserID;
+        this.UserClass = UserClass;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public User(int UserID, String firstname, String lastname, int phone, String email){
+        this.UserID = UserID;
+        this.UserClass = KUNDE;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getUserID(){
+        return UserID;
+    }
+
+    public int getUserClass(){
+        return UserClass;
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+
+    public int getPhone(){
+        return phone;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+
+}
