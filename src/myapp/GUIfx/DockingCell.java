405aac27e5f8889f449f2cebf3da4153d2ab7fe2
sarahjel <sarahjel@stud.ntnu.no>
2018-04-04 13:01:50 +0200

    Merge GUI branch into master

---
+++ src/myapp/GUIfx/DockingCell.java
@@ -0,0 +1,43 @@
+package myapp.GUIfx;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+import myapp.data.Bike;
+import myapp.data.Docking;
+
+import java.util.HashMap;
+
+public class DockingCell extends ListCell<Docking>{
+    private HBox hBox;
+    private Label name;
+    //private Label location;
+    private Label capacity;
+    private Label numberOfBikes;
+
+    public DockingCell(){
+        hBox = new HBox(100);
+        name = new Label();
+        //location = new Label();
+        capacity = new Label();
+        //numberOfBikes = new Label();
+        hBox.getChildren().addAll(name, capacity);
+    }
+
+    @Override
+    public void updateItem(Docking item, boolean empty){
+        super.updateItem(item, empty);
+        if(item != null && !empty){
+            name.setText(item.getName());
+            capacity.setText(Integer.toString(item.getCapacity()));
+
+            /*int bikes = 0;
+            HashMap<Integer, Bike> bikesAtStation = new HashMap<>(item.getCapacity());
+            for(int i = 0; i < bikesAtStation.size(); i++){
+                bikes = bikes + 1;
+            }
+            numberOfBikes.setText(Integer.toString(bikes));*/
+            setGraphic(hBox);
+        }
+    }
+}
