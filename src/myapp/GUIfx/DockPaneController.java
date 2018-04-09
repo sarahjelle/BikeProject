56af4022adadc067e1a656028b145d438cb3d04d
sindretoftnordal <sindretn@stud.ntnu.no>
2018-04-09 13:55:56 +0200

    Location adapt

--- src/myapp/GUIfx/DockPaneController.java
+++ src/myapp/GUIfx/DockPaneController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import myapp.data.Docking;
 
 public class DockPaneController {
     @FXML private TableView<Docking> docktable;
