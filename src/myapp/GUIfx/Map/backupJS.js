2cc9fa5316031823ac8dbeba6be933e8fc47f3d8
Martin Moan <martimoa@stud.iie.ntnu.no>
2018-04-10 11:58:18 +0200

    Edit js functionality to MapController

--- src/myapp/GUIfx/Map/backupJS.js
+++ src/myapp/GUIfx/Map/backupJS.js
@@ -1,6 +1,8 @@
+//importClass(myapp.map.DummyLocations);
+//var bike1 = new DummyLocations(123, 63.429, 10.3969);
 var map;
 function initMap() {
-    var positions = [{lat: 63.4507053, lng: 10.3826452},{lat: 63.4283511, lng: 10.3879615}];
+    var positions = [{lat: 63.429148, lng: 10.392461},{lat: 63.427189, lng: 10.396924},{lat: 63.429319, lng: 10.386667},{lat: 63.431948, lng: 10.397567}];
     var cetnterPos = {lat: 63.429148, lng: 10.392461};
 
     var options = {
