d23fe2cafcec23e7fe6d5fb4ff0d51d5fc50f046
fredrikmedia <fredrmed@stud.ntnu.no>
2018-04-10 11:05:08 +0200

    Correcting Docking.java

--- src/myapp/data/Docking.java
+++ src/myapp/data/Docking.java
@@ -18,7 +18,7 @@ public class Docking {
 
     DBH dbh = new DBH();
 
-    public Docking(int id, String name, Location location, int capacity){
+    public Docking(int id, String name, Location location, int capacity) {
         this.id = id;
         this.name = name;
         this.location = location;
@@ -30,11 +30,11 @@ public class Docking {
         return id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String newName){
+    public void setName(String newName) {
         this.name = newName;
     }
 
@@ -46,24 +46,32 @@ public class Docking {
         this.location = location;
     }
 
-    public int getCapacity(){
+    public int getCapacity() {
         return capacity;
     }
 
-    public int getOpenSpaces(){
-        return getCapacity() - bikes.size();
+    public int getOpenSpaces() {
+        return getCapacity() - openSpaces();
     }
 
-    public void addBike(Bike bike){
+    public void addBike(Bike bike) {
         int spot = firstOpen();
         if(spot >= 0){
             bikes.add(spot, bike);
-            //dbh.dockBike(id, spot, bike);
+            dbh.dockBike(id, spot, bike);
+        }
+    }
+
+    // To use when creating docking station from DB
+    public void forceAddBike(Bike bike, int spot) {
+        spot--;
+        if(spot >= 0){
+            bikes.add(spot, bike);
         }
     }
 
     //Helper function for finding first open spot
-    public int firstOpen(){
+    public int firstOpen() {
         int i = 0;
         while(bikes.get(i) != null){
             i++;
@@ -74,7 +82,7 @@ public class Docking {
         return -1;
     }
 
-    public int openSpaces(){
+    public int openSpaces() {
         int count = 0;
         for (int i = 0; i < bikes.size(); i++){
             if(bikes.get(i) == null){
@@ -84,8 +92,14 @@ public class Docking {
         return count;
     }
 
-    public boolean removeBike(int bikeId){
-        return (bikes.remove(bikeId) != null);
+    public boolean removeBike(int bikeId) {
+        for(int i = 0; i < bikes.size(); i++) {
+            if(bikes.get(i).getId() == bikeId) {
+                bikes.add(i, null);
+                return true;
+            }
+        }
+        return false;
     }
 
     public ArrayList<Bike> getBikes() {
