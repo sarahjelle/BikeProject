47514009ec610bc9d0db4e26de55eb5f25585707
Martin Moan <martimoa@stud.iie.ntnu.no>
2018-04-19 14:13:48 +0200

    IMPORTANT

--- src/myapp/GUIfx/Statistic/GenerateStats.java
+++ src/myapp/GUIfx/Statistic/GenerateStats.java
@@ -4,7 +4,9 @@ import myapp.data.Bike;
 import myapp.data.Docking;
 import myapp.dbhandler.DBH;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
  Status:
@@ -17,12 +19,17 @@ public class GenerateStats {
     private DBH dbh;
     private ArrayList<Bike> bikes;
     private ArrayList<Docking> docks;
+    private DataUpdater du;
+    private Thread duThread;
 
     public GenerateStats() {
         this.dbh = new DBH();
         this.bikes = null;
         this.bikes = dbh.getAllBikes();
-        this.docks = dbh.getAllDockingStations();
+        this.docks = new ArrayList<Docking>(Arrays.asList(dbh.getAllDockingStationsWithBikes()));
+        this.du = new DataUpdater();
+        this.duThread = new Thread(du);
+        duThread.start();
     }
 
     public String[] dockingName() {
@@ -30,13 +37,13 @@ public class GenerateStats {
         for (int i=0; i<docks.size(); i++) {
             dockName[i] = docks.get(i).getName().split(",")[0];
         }
-      return dockName;
+        return dockName;
     }
 
     public int[] dockCapacity(){
         int[] dockCap = new int[docks.size()];
         for (int i=0; i<docks.size(); i++) {
-            dockCap[i] = docks.get(i).getCapacity();
+            dockCap[i] = docks.get(i).getUsedSpaces();
         }
         return dockCap;
     }
@@ -44,13 +51,13 @@ public class GenerateStats {
     public int[] bikeAvailability(){
         int[] availStats = new int[3];
         for (Bike b : bikes){
-            if (b.getStatus()==1){
+            if (b.getStatus() == 1){
                 availStats[0]++;
             }
-            if (b.getStatus()==2){
+            if (b.getStatus() == 2){
                 availStats[1]++;
             }
-            if (b.getStatus()==3){
+            if (b.getStatus() == 3){
                 availStats[2]++;
             }
         }
@@ -66,5 +73,32 @@ public class GenerateStats {
             bikeStatistics[3][i]=(int)bikes.get(i).getBatteryPercentage();
         }
         return bikeStatistics;
+    }
+
+    class DataUpdater implements Runnable{
+        public Boolean stop = false;
+        private int UPDATE_INTERVAL = 5000; // ms
+        public DataUpdater(){}
+
+        public DataUpdater(int UPDATE_INTERVAL){
+            this.UPDATE_INTERVAL = UPDATE_INTERVAL;
+        }
+
+        public void run(){
+            while(!stop){
+                //Do work
+                bikes = dbh.getAllBikes();
+                docks = new ArrayList<>(Arrays.asList(dbh.getAllDockingStationsWithBikes()));
+                try{
+                    Thread.sleep(UPDATE_INTERVAL);
+                } catch(InterruptedException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        public void stop(){
+            this.stop = true;
+        }
     }
 }
